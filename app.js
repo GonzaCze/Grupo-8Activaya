239cccc050b5dbcb9ffe4b7b0d6de6a0b10097b8
Sergio D'Angelo <sergioddangelo@gmail.com>
2022-05-26 22:43:32 -0300

    vinculacion platillasy correccion de pasajes urls

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const path =  require ("path")
-
 const app = express ();
-
+const path =  require ("path");
 const publicPath = path.resolve(__dirname, './public');
 app.use (express.static(publicPath))
-
 app.listen(3000, ()=> {
     console.log("Servidor corriendo en el puerto 3000")
 })
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+app.get('/registro', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/registro.html'))
+})
+app.get('/detalle', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/detalle.html'))
+})
+app.get('/carrito', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/carrito.html'))
 })
